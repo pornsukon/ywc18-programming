6aa898777ff3aa583a2ae3709a248e69272364e9
pornsukon <pornsukon@kkumail.com>
2020-11-15 22:42:58 +0700

    Updates

---
+++ precache-manifest.c63ef94ebffdd541e1517d61b8b802b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f000fabf2fa9c08968fe6b1fb9ac39",
+    "url": "/ywc18-programming/index.html"
+  },
+  {
+    "revision": "962cd85e71eacc7bd79a",
+    "url": "/ywc18-programming/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a46617966b536919ab2",
+    "url": "/ywc18-programming/static/js/2.68f56c50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ywc18-programming/static/js/2.68f56c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962cd85e71eacc7bd79a",
+    "url": "/ywc18-programming/static/js/main.33b346b1.chunk.js"
+  },
+  {
+    "revision": "7d71f67014277a9f94b0",
+    "url": "/ywc18-programming/static/js/runtime-main.fc7d86f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ywc18-programming/static/media/logo.5d5d9eef.svg"
+  }
+]);
