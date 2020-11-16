e11187ad827b5fd1d5b4c3279ec061f5fea7fc77
pornsukon <pornsukon@kkumail.com>
2020-11-16 11:52:31 +0700

    [ADD] hello world

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <p>Hello World</p>
   </React.StrictMode>,
   document.getElementById('root')
 );
